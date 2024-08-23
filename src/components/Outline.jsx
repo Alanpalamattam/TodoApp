07114b1b7b77ef60eaf3f42c4b68928f6fbdb0c1
Alan Peter <alanpalamattam2@gmail.com>
2024-08-23 23:47:45 +0530

    3rd commit

--- src/components/Outline.jsx
+++ src/components/Outline.jsx
@@ -2,7 +2,7 @@ import styles from "./Outline.module.css";
 export default function Outline() {
   return (
     <div>
-      <h2 className={styles.header}>Outline Component</h2>
+      <h2 className={styles.header}>Outline Component example</h2>
     </div>
   );
 }
