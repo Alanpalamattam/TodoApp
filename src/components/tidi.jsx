c2ebd77b054e9bb294313eb8f4e0ca37c4867e44
Alan Peter <alanpalamattam2@gmail.com>
2024-09-07 23:50:32 +0530

    commit

---
+++ src/components/tidi.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import Todoapplist from "./todoapplist";
+
+export default function Todo() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    console.log(todos);
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setTodo(e.target.value)}
+          type="text"
+          value={todo}
+        />
+        <button type="submit">Add</button>
+      </form>
+      {todos.map((item) => (
+        <Todoapplist key={item} item={item} />
+      ))}
+    </div>
+  );
+}
