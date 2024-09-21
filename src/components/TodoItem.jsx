dcc1446097dbaff705ea52cf82955aaeceb77cd4
Alan Peter <alanpalamattam2@gmail.com>
2024-09-21 22:37:34 +0530

    commit

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,7 +2,12 @@ import styles from "./todoitem.module.css";
 export default function Todoapplist({ item }) {
   return (
     <div clasname={styles.item}>
-      <div className={styles.itemName}>{item}</div>
+      <div className={styles.itemName}>
+        {item}
+        <span>
+          <button className={styles.deletebutton}>X</button>
+        </span>
+      </div>
       <hr className={styles.line} />
     </div>
   );
