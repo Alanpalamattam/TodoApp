02b0efe5418db467160691ab2d399d978a0651e0
Alan Peter <alanpalamattam2@gmail.com>
2024-09-27 23:45:16 +0530

    commit

--- src/components/Todotest.tsx
+++ src/components/Todotest.tsx
@@ -1,20 +1,5 @@
 import React from "react";
 
 export function test() {
-  return (
-    <form className={styles.todoform} onSubmit={handleSubmit}>
-      <div className={styles.inputContainer}>
-        <input
-          className={styles.modernInput}
-          onChange={(e) => setTodo(e.target.value)}
-          type="text"
-          value={todo}
-          placeholder="Enter todo"
-        />
-        <button className={styles.modernButton} type="submit">
-          Add Todo
-        </button>
-      </div>
-    </form>
-  );
+  return console.log("gep");
 }
